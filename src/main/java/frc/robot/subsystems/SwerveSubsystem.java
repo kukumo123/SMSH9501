7f1ac4d915c0f546a599a06b3ad30b4e9e474688
kukumo123 <106147327+kukumo123@users.noreply.github.com>
2024-04-05 15:19:17 +0800

    Update SwerveSubsystem.java

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 
+import com.ctre.phoenix6.hardware.CANcoder;
 import com.kauailabs.navx.frc.AHRS;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.path.PathPlannerPath;
@@ -11,14 +12,17 @@ import com.pathplanner.lib.util.ReplanningConfig;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.Odometry;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.proto.Kinematics;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.StructArrayPublisher;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -28,14 +32,15 @@ import frc.robot.Constants.DriveConstants;
 
 public class SwerveSubsystem extends SubsystemBase {
 
-    SwerveModuleState[] states = new SwerveModuleState[] {
-        new SwerveModuleState(),
-        new SwerveModuleState(),
-        new SwerveModuleState(),
-        new SwerveModuleState()
-    };
-    StructArrayPublisher<SwerveModuleState> publisher = NetworkTableInstance.getDefault()
-    .getStructArrayTopic("MyStates", SwerveModuleState.struct).publish();
+    private final CANcoder FLcancoder = new CANcoder(9, "rio");
+    private final CANcoder BLcancoder = new CANcoder(10, "rio");
+    private final CANcoder FRcancoder = new CANcoder(11, "rio");
+    private final CANcoder BRcancoder = new CANcoder(12, "rio");
+    private Field2d field = new Field2d();
+    private SwerveDriveOdometry odometry;
+    private SwerveDriveKinematics kinematics;
+
+
     
 
     private final SwerveModule frontLeft = new SwerveModule(
@@ -87,7 +92,6 @@ public class SwerveSubsystem extends SubsystemBase {
         }).start();
         setupPathPlanner();
     };
-    
 
     public void zeroHeading() {
         gyro.reset();
@@ -106,15 +110,15 @@ public class SwerveSubsystem extends SubsystemBase {
     }
 
     public void resetOdometry(Pose2d pose) {
-        odometer.resetPosition(getRotation2d(), getModulePosition(), pose);
+        odometer.resetPosition(gyro.getRotation2d(), getModulePosition(), pose);
     }
 
     @Override
     public void periodic() {
-        odometer.update(getRotation2d(), getModulePosition());
+        field.setRobotPose(getPose());
+        odometer.update(gyro.getRotation2d(), getModulePosition());
         SmartDashboard.putNumber("Robot Heading", getHeading());
         SmartDashboard.putString("Robot Location", getPose().getTranslation().toString());
-        
     }
 
     public void stopModules() {
@@ -160,6 +164,11 @@ public class SwerveSubsystem extends SubsystemBase {
     }
 
     public void getTurningEncoderPosition(){
+        SmartDashboard.putNumber("FL", FLcancoder.getAbsolutePosition().getValue()*(Math.PI*2));
+        SmartDashboard.putNumber("BL", BLcancoder.getAbsolutePosition().getValue()*(Math.PI*2));
+        SmartDashboard.putNumber("FR", FRcancoder.getAbsolutePosition().getValue()*(Math.PI*2));
+        SmartDashboard.putNumber("BR", BRcancoder.getAbsolutePosition().getValue()*(Math.PI*2));
+
         SmartDashboard.putNumber("FrontLeftEncoder", frontLeft.getTurningPosition());
         SmartDashboard.putNumber("BackLeftEncoder", backLeft.getTurningPosition());
         SmartDashboard.putNumber("FrontRightEncoder", frontRight.getTurningPosition());
@@ -176,15 +185,23 @@ public class SwerveSubsystem extends SubsystemBase {
         setModuleStates(moduleStates);
     }
 
+    public void resetPose(Pose2d pose) {
+        odometry.resetPosition(gyro.getRotation2d(), getModulePosition(), pose);
+    }
+
+    public ChassisSpeeds getSpeeds(){
+        return kinematics.toChassisSpeeds(getSweveModuleStates());
+    }
+
     public void setupPathPlanner()
     {
         AutoBuilder.configureHolonomic(
             this::getPose,
-            this::resetOdometry,
-            this::getRobotVelocity, 
+            this::resetPose,
+            this::getRobotVelocity,     
             this::driveChassisSpeeds,
             new HolonomicPathFollowerConfig( 
-                                            new PIDConstants( 0.03, 0, 0, 0),
+                                            new PIDConstants( 0.01, 0, 0, 0),
                                             new PIDConstants(0.03, 0, 0, 0),
                                             Constants.DriveConstants.kTeleDriveMaxSpeedMetersPerSecond,
                                             0.2,
@@ -207,12 +224,10 @@ public class SwerveSubsystem extends SubsystemBase {
     public Command getAutonomousCommand(String pathName, boolean setOdomToStart)
     {
       PathPlannerPath path = PathPlannerPath.fromPathFile(pathName);
-  
       if (setOdomToStart)
       {
         resetOdometry(new Pose2d());
       }
-  
       return AutoBuilder.followPath(path);
     }
 }
